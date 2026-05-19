39f4a359c0b2ec464f771ffe6ed32bd580f9a34b
I Komang Radita Suardhana <raditagit04@gmail.com>
2026-05-19 16:06:56 +0800

    Initial frontend project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className='flex h-screen w-screen items-center justify-center bg-slate-900'>
+        <h1 className='text-9xl font-bold text-white'>hello word</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
